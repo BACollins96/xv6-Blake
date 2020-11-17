993f1a9997ba91d252a325617339accb9527b9fa
BACollins96 <BACollins1@csustudent.net>
2020-11-16 23:25:07 -0500

    First update, messing with fork

--- forktest.c
+++ forktest.c
@@ -4,6 +4,39 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "param.h"
+#include "types.h"
+//#include "defs.h"
+
+uint
+random(void)
+{
+  // Take from http://stackoverflow.com/questions/1167253/implementation-of-rand
+  static unsigned int z1 = 12345, z2 = 12345, z3 = 12345, z4 = 12345;
+  unsigned int b;
+  b  = ((z1 << 6) ^ z1) >> 13;
+  z1 = ((z1 & 4294967294U) << 18) ^ b;
+  b  = ((z2 << 2) ^ z2) >> 27;
+  z2 = ((z2 & 4294967288U) << 2) ^ b;
+  b  = ((z3 << 13) ^ z3) >> 21;
+  z3 = ((z3 & 4294967280U) << 7) ^ b;
+  b  = ((z4 << 3) ^ z4) >> 12;
+  z4 = ((z4 & 4294967168U) << 13) ^ b;
+
+  return (z1 ^ z2 ^ z3 ^ z4) / 2;
+}
+
+int
+randomrange(int lo, int hi)
+{
+  if (hi < lo) {
+    int tmp = lo;
+    lo = hi;
+    hi = tmp;
+  }
+  int range = hi - lo + 1;
+  return random() % (range) + lo;
+}
 
 #define N  1000
 
@@ -17,6 +50,8 @@ void
 forktest(void)
 {
   int n, pid;
+
+  //size = atoi(argv[1]);
 
   printf(1, "fork test\n");
 
@@ -49,8 +84,12 @@ forktest(void)
 }
 
 int
-main(void)
+main(int argc, char *argv[])
 {
+
+  int size = atoi(argv[1]);
+  size = size +1;
+  printf(1, "fork test OK\n");
   forktest();
   exit();
 }
